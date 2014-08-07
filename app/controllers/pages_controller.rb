6f73a67e65c3bef7a81ab01c07b77803b1dfdff6
Bryan Jenkins <bryancjenkins@gmail.com>
2014-08-07 17:46:18 -0400

    Haven't tested but try something like this

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,35 @@
 class PagesController < ApplicationController
   before_filter :authenticate_user!
+
+  before_action :set_page, only: [:show, :store_pagetime]
+
   def index
   	@page = Page.all
   end
 
   def show
-  	@page = Page.find(params[:id])
+    @pagetime = Page.userlog.build
   end
 
   def create
-
-	  @pagetime = Userlog.new(params[:userlog])
-	  @pagetime.user_id = current_user
-      @pagetime.save
-	  
+	  # @pagetime = Userlog.new(params[:userlog]) #wrong
+	  # @pagetime.user_id = current_user
+    # @pagetime.save
   end
-  
+
+  def store_pagetime
+    @pagetime = Page.userlog.build(pagetime_params)
+    @pagetime.user = current_user
+    @pagetime.save
+  end
+
+  private
+
+  def set_page
+    @page = Page.find(params[:id])
+  end
+
+  def pagetime_params
+     params.require(:page).permit(pagetime_attributes: [:id, :starttime, :endtime] )
+  end
 end
